0d8c10b9ab296e4ecbead0e55b11bf55bbce0fa3
Darryl Walker <darryl.walker@jacobs.com>
2022-04-11 12:44:55 +0100

    Working selenium test

---
+++ src/__tests__/selenium.test.js
@@ -0,0 +1,25 @@
+import webdriver from 'selenium-webdriver';
+import ChromeSelenium from 'selenium-webdriver/chrome';
+import 'chromedriver';
+
+const serverUri = 'http://localhost:3000';
+const appTitle = 'Dark Mode Challenge';
+
+const options = new ChromeSelenium.Options();
+
+options.addArguments('--headless');
+options.addArguments('--no-sandbox');
+options.addArguments('--disable-dev-shm-usage');
+
+const driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .setChromeOptions(options)
+  .build();
+
+it('returns the localhost app title', async () => {
+  const page = await driver.get(serverUri);
+
+  const title = await driver.getTitle();
+
+  expect(title).toBe(appTitle);
+});
