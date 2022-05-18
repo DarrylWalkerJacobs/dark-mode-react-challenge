51346c7c54d596df2423a078bdb614cc8dbd09af
Stephen Turner <Stephen.Turner@jacobs.com>
2022-05-18 17:31:04 +0100

    Manoj interview

--- src/__tests__/selenium.test.js
+++ src/__tests__/selenium.test.js
@@ -4,7 +4,7 @@ import ChromeSelenium from 'selenium-webdriver/chrome';
 import 'chromedriver';
 
 const serverUri = 'http://localhost:3000';
-const appTitle = 'Dark Mode Challenge';
+const appTitle = 'Dark Mode ';
 
 const options = new ChromeSelenium.Options();
 
@@ -28,7 +28,19 @@ it('has the correct application title', async () => {
 it('changes the dark mode icon tooltip text when clicked ', async () => {
   await driver.get(serverUri);
      
-  //
+  const driver = new chromedriver();
+  driver.get("https://625457911794750cc4c7545f--the-awesome-darrylwalkerjacobs-site.netlify.app/");
+
+  driver.findElement(By.xpath('//*[@class="app__dark-mode-btn icon level-right"]')).click();
+  thread.sleep(5000);
+  Actions action= new Actions(driver);
+  action.
+
+
+  driver.findElement(By.xpath('//*[@class="app__dark-mode-btn icon level-right"]')).getText();
+
+  
+
 
   const buttonTitlePre = await //
   expect(buttonTitlePre).toBe("Set dark mode");
